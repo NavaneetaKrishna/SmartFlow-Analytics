27d518f8f2cd38907c6ea6e78104114262f80bed
Navaneeta Krishna <navaneetakrishna10d@gmail.com>
2026-05-04 16:21:54 +0530

    Setup and requirements have been added

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from typing import List
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path):
+    requirements = []
+    with open(file_path) as file_obj:
+        # 1. Read all lines
+        lines = file_obj.readlines()
+        
+        # 2. Clean lines (remove newlines and extra spaces)
+        requirements = [req.strip() for req in lines]
+
+        # 3. Filter out the '-e .' trigger 
+        # This creates a new list without the offending string
+        requirements = [req for req in requirements if req != HYPHEN_E_DOT]
+            
+    return requirements
+setup(
+    name='SmartFlow-Analytics',
+    version='1.0.0',
+    author='Navaneeta Krishna',
+    author_email='navaneetakrishna10d@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
